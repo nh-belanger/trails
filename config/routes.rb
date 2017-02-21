be98256dbb09c14e0794ccd9b3cef836d06dde11
nh-belanger <nick.h.belanger@gmail.com>
2017-02-21 18:29:40 -0500

    initial styling and more MVC work

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
+  get "/pages/:page" => "pages#show"
+  
   resources :users
   resources :trails
+
+  root 'pages#show', page: 'home'
 end
