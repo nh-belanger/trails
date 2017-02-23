f566095f197c727920176aadcd6dddc9859aa5cc
nh-belanger <nick.h.belanger@gmail.com>
2017-02-22 19:50:55 -0500

    still trying to make the background work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,10 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Hopefully this makes the background images work
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.assets.compile = true
+  config.assets.digest = true
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -27,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
