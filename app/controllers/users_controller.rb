40b01a118010d7f32769f7b734e0e29a0492b169
nh-belanger <nick.h.belanger@gmail.com>
2017-04-02 15:48:35 -0400

    adding user roles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name)
+    params.require(:user).permit(:first_name, :last_name, :role)
   end
 end
