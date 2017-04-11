648e273e378dcec718c9efae5810ff1137d4ad44
nh-belanger <nick.h.belanger@gmail.com>
2017-04-11 15:22:13 -0400

    styling fix

--- db/migrate/20170407001343_add_avatar_to_users.rb
+++ db/migrate/20170407001343_add_avatar_to_users.rb
@@ -1,7 +1,6 @@
 class AddAvatarToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :profile_picture, :string
-
     add_column :trails, :picture, :string
   end
 end
--- db/migrate/20170407014935_add_picture_to_trails.rb
+++
@@ -1,6 +0,0 @@
-class AddPictureToTrails < ActiveRecord::Migration[5.0]
-  def change
-
-        add_column :trails, :picture, :string
-  end
-end
